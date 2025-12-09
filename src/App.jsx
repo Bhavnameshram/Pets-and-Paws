83e3173b3b2bd66f1e690d8707f4416273d552e4
Shivallll <shivalmehere5@gmail.com>
2025-12-09 17:43:34 +0530

    start

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Components/index.jsx'
+
+function App() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/Components/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function index() {
+  return (
+    <>
+    <div className='h-60 w-50 bg-white'>
+        
+     </div>
+        </>
+  )
+}
+
+export default index
