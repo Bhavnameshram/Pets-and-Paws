7c96edf741ad1ec7ae01d7a8f21de03540816a56
stepee sondawale <steffisondawale06@gmail.com>
2025-12-16 21:46:22 +0530

    feedback page is here.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 // import './Components/index.jsx'
 import Adopt from './Components/Adopt.jsx'
+import Feedback from './Components/Feedback.jsx'
 
 function App() {
   return (
-   <Adopt />
+    <div>   
+      <Adopt />
+   <Feedback />
+   </div>
+
   )
 }
 
---
+++ src/Components/Feedback.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Feedback() {
+  return (
+   <>
+   <div>
+    <h1>Feedback Page</h1>
+    <p>We value your feedback! Please let us know your thoughts about our services.</p>
+   </div>
+   </>
+  )
+}
+
+export default Feedback
