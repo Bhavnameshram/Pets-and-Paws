2b4eea25484ddbf34fe2635d9e572e1d222aea34
dhanasvi22 <shendedhanasvi@gmail.com>
2025-12-16 01:06:23 -0500

    import video

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Footer from './Components/Footer.jsx'
+import Video from './Components/video.jsx'
 
 function App() {
   return (
     <div>
+      <Video/>
       <Footer/>
     
     </div>
