cfa0f15c11eac56d190ebd34f99288c71a534086
dhanasvi22 <shendedhanasvi@gmail.com>
2025-12-16 01:40:11 -0500

    components create

--- src/Components/video.jsx
+++ src/Components/video.jsx
@@ -2,19 +2,19 @@ import React from 'react'
 import video from '../assets/WhatsApp Video 2025-12-15 at 3.20.22 PM (1).mp4'
 
 function Video() {
-  <>
-    <div className="w-full">
+  return (
+    <div className="w-full bac" >
       <video
         src={video}
         autoPlay
         muted
         loop
-        controls
-        className="w-full h-full object-cover rounded-lg shadow"
+        // controls
+        className="w-{650} h-{350px} ml-95 justify-center items-center object-cover shadow"
       />
         </div>
 
-  </>
+  )
 }
 
 export default Video
