8a3822adee683b34acde590d39fb5413885bf41c
poonam-243 <poonambawane828@gmail.com>
2025-12-17 21:14:50 +0530

    poonam

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './Components/index.jsx'
-
+import Adoption from './Components/Adoption.jsx'
 function App() {
   return (
     <div>
-      
+      <Adoption/>
     </div>
   )
 }
---
+++ src/Components/Adoption.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Adoption() {
+  return (
+    <div className="h-screen w-screen bg-black display-grid">
+      <h1 className="text-3xl font-bold text-white items-center flex justify-center mt-50px">Pet Adoption Form</h1>
+
+    </div>
+  )
+}
+
+export default Adoption
