3f9a962b31318e187ec78c98b46728eb0f962f4a
dhanasvi22 <shendedhanasvi@gmail.com>
2025-12-18 02:32:39 -0500

    Add buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Footer from './Components/Footer.jsx'
 import Video from './Components/video.jsx'
+import Button from './Components/Button.jsx'
 
 function App() {
   return (
     <div>
+      <Button/>
       <Video/>
       <Footer/>
     
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 function Button() {
   return (
-    <div>Button</div>
+    <>
+    <div className="bg-[#f5cf7b] flex justify-center items-center gap-5 text-2xl">
+        <button className="bg-[#ca9643] text-[#4A2C16] font-bold rounded py-2 px-4 mt-2.5 mb-2.5">Pets Waiting For You!</button>
+        <button className="bg-[#ca9643] text-[#4A2C16] font-bold rounded py-2 px-4 mt-2.5 mb-2.5">Donate Your Pets</button>
+
+    </div>
+    </>
   )
 }
 
