a93033d9ca1650fd4c8054992850ea494771a00e
dhanasvi22 <shendedhanasvi@gmail.com>
2025-12-18 02:08:33 -0500

    footer done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -15,7 +15,7 @@ function Footer() {
     
     <div>
       <h2 className="text-2xl font-extrabold mb-3 flex gap-2 items-center">Paws & Pet <MdOutlinePets /></h2>
-      <p className="text-sm leading-relaxed">
+      <p className="text-[14px] ">
         Paws & Pet is dedicated to giving love, care, and forever homes to pets.
         Adopt, donate, and help us make their lives better.
       </p>
@@ -24,7 +24,7 @@ function Footer() {
     {/* Quick Links */}
     <div>
       <h3 className="font-bold mb-3">Quick Links</h3>
-      <ul className="space-y-2 text-sm">
+      <ul className="space-y-2 text-[14px]">
         <li className="cursor-pointer">Home</li>
         <li className="cursor-pointer">Adopt a Pet</li>
         <li className="cursor-pointer">Donate</li>
@@ -37,7 +37,7 @@ function Footer() {
     {/* Services */}
     <div>
       <h3 className="font-bold mb-3">Our Services</h3>
-      <ul className="space-y-2 text-sm">
+      <ul className="space-y-2 text-[14px]">
         <li>Pet Adoption</li>
         <li>Pet Donation</li>
         <li>Pet Accessories</li>
@@ -50,28 +50,28 @@ function Footer() {
     
     <div>
       <h3 className="font-bold mb-3">Contact Us</h3>
-      <ul className="space-y-2 text-sm">
-        <li>📍 Nagpur, India</li>
-        <li>📞 +91 98765 43210</li>
-        <li>✉ pawspet@gmail.com</li>
-        <li>🕒 Mon – Sat : 9 AM - 6 PM</li>
+      <ul className="space-y-2 text-[14px]">
+        <li>Location - Nagpur, India</li>
+        <li>Contact - +91 1234567890</li>
+        <li>Email - pawspet@gmail.com</li>
+        <li>Time - Mon – Sat : 9 AM - 6 PM</li>
       </ul>
     </div>
 
   </div>
 
   {/* Quote */}
-  <div className="text-center px-6 py-4 text-sm font-medium bg-[#D9A441]">
+  <div className="text-center px-6 py-4 text-[14px] font-medium bg-[#D9A441]">
      “Adopt, don’t shop. They deserve to be loved too.”
   </div>
 
   {/* Social Icons */}
   <div className="flex justify-center py-2 gap-6 text-4xl">
-    <span className="cursor-pointer hover:scale-110 transition"><FaInstagram /></span>
-    <span className="cursor-pointer hover:scale-110 transition"><RiFacebookCircleLine /></span>
+    <span className="cursor-pointer hover:scale-110 transition"><a href="https://www.instagram.com/adoptapet/?hl=en"><FaInstagram /></a></span>
+    <span className="cursor-pointer hover:scale-110 transition"><a href="https://www.facebook.com/adoptapet"><RiFacebookCircleLine /></a></span>
   </div>
 
-  <div className="text-center text-sm py-3 bg-[#B8743B] text-white">
+  <div className="text-center text-[14px] py-3 bg-[#B8743B] text-white">
     © 2025 Paws & Pet. All Rights Reserved.
   </div>
 
