6fd4e7d19f56be7c2beb07fa1609af14b869bd56
Shivallll <shivalmehere5@gmail.com>
2025-12-19 15:08:08 +0530

    my local changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import React from 'react'
-<<<<<<< HEAD
-// import Index from "./Components/Index.jsx"
 import Index from "./Components/Index.jsx"
+import Adopt from './Components/Adopt.jsx'
+
 
 function App() {
   return (
     <div>
+      
       <Index />
-      {/* <Home/> */}
+      <Adopt/>
+      
       
     </div>
-=======
-// import index from './Components/index.jsx'
-import Adopt from './Components/Adopt.jsx'
-
-function App() {
-  return (
-   <Adopt />
-   
->>>>>>> d95ca6f392ff1a5abbfdbf60f7d2aa9fbb43365b
   )
 }
 
-export default App
+
+export default App;
